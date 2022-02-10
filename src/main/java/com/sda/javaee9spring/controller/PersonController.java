63dce9d19e29433015b71d0a5fa86286824ed564
Jana Pulk <pulk.jana@gmail.com>
2022-02-10 15:31:45 +0200

    [change] - some var examples inside PersonController

--- src/main/java/com/sda/javaee9spring/controller/PersonController.java
+++ src/main/java/com/sda/javaee9spring/controller/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -19,6 +20,16 @@ public class PersonController {
 
     @GetMapping("/names")
   public String showListOfPersonsNames(Model data){
+        //with var Java is going to guess type of the variable based on assigned value
+        //var name = "Jana"; //String name = "Jana";
+        ArrayList<Person> myBestFriends = new ArrayList<Person>(); // till Java 7
+        ArrayList<Person> myBestFriendsJava7 = new ArrayList<>(); //since Java 7
+        var myBestFriendsJava10 = new ArrayList<Person>(); //since Java 10
+        var myBestFriendsJava10v2 = new ArrayList<>(); //since Java 10
+        var myBesFriendsJava10v3 = new ArrayList<Object>(); //since Java 10
+
+
+        //this list will be dynamic in the future
       //  List<Person> persons
         var persons
         = List.of(
