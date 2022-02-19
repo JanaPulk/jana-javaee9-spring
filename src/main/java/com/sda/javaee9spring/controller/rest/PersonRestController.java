3a31b01df579bc0fb8f9524ba9f96b509f6fd779
Jana Pulk <pulk.jana@gmail.com>
2022-02-19 12:07:23 +0200

    [refactor] - changing persons/{id} endpoints si its uses lambdas

--- src/main/java/com/sda/javaee9spring/controller/rest/PersonRestController.java
+++ src/main/java/com/sda/javaee9spring/controller/rest/PersonRestController.java
@@ -4,12 +4,14 @@ import com.sda.javaee9spring.entity.PersonEntity;
 import com.sda.javaee9spring.service.RealPersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Slf4j
@@ -24,21 +26,31 @@ public class PersonRestController {
     }
 
 
-        @GetMapping("/persons")
-        public List<PersonEntity> findAllPersons() {
-            log.info("findAllPersons()");
+    @GetMapping("/persons")
+    public List<PersonEntity> findAllPersons() {
+        log.info("findAllPersons()");
 
-            return realPersonService.readAllPersonEntities();
+        return realPersonService.readAllPersonEntities();
 
     }
-
     @GetMapping("/persons/{id}")
-    public PersonEntity findPersonEntityById(@PathVariable("id") Long personId){
+    public ResponseEntity<PersonEntity> findPersonEntityById(@PathVariable("id") Long personId) {
         log.info("trying to find person entity by id: [{}]", personId);
-
-        return realPersonService.readPersonEntityById(personId);
-
-
+        var personEntity = personService.readPersonEntityById(personId);
+        return personEntity.map(personEntity1 -> ResponseEntity.ok(personEntity1)) // Optional<PersonEntity> -> Optional<ResponseEntity<PersonEntity>>
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
-}
+
+
+
+
+
+  //      if (personEntity.isEmpty()) {
+  //          PersonEntity personEntityFromOptional = personEntity.get();
+  //          return ResponseEntity.notFound().build();
+        }
+//            return new ResponseEntity<>(null, null, HttpStatus.NOT_FOUND);;
+
+  //      return ResponseEntity.ok(personEntity);
+//        return new ResponseEntity<>(personEntity, null, HttpStatus.OK);
 
